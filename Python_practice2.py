e15bd98027aa2766444700a6e13fafb082012790
TracyNK <95154345+TracyKien@users.noreply.github.com>
2022-02-06 21:40:02 -0600

    Add files via upload

---
+++ Python_practice2.py
@@ -0,0 +1,76 @@
+print("Hello World")
+print(type(3))
+print(type(2019))
+ballots = 1,341
+print(ballots)
+print(type(ballots))
+print(type(73.81))
+print(type("Hello World"))
+print(type(True))
+
+print(5 + 2 * 3)
+print(8 // 5 - 3)
+print(8 + 22 * 2 - 4)
+print(16 - 3 / 2 + 7 - 1)
+print(3 ** 3 % 5)
+print(5 + 9 * 3 / 2 - 4)
+print((5 + 2) * 3)
+print((8 // 5) - 3)
+print(8 + (22 * (2 - 4)))
+print(16 - 3 / (2 + 7) - 1)
+print(3 ** (3 % 5))
+print(5 + ( 9* 3 / 2 - 4))
+print(5 + (9 * 3 / (2 - 4)))
+
+# How many votes did you get?
+#my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+#total_votes = int(input("What is the total votes in the election? "))
+# Calculate the percentage of votes you received.
+#percentage_votes = (my_votes / total_votes) * 100
+#print("I received " + str(percentage_votes)+"% of the total votes.")
+
+#temperature = int(input("What is the temperature outside? "))
+
+#if temperature > 80:
+    #print("Turn on the AC.")
+#else:
+    #print("Open the windows.")
+
+  
+    #What is the score?
+#score = int(input("What is your test score? "))
+
+# Determine the grade.
+#if score >= 90:
+    #print('Your grade is an A.')
+#else:
+    #if score >= 80:
+        #print('Your grade is a B.')
+    #else:
+        #if score >= 70:
+            #print('Your grade is a C.')
+        #else:
+            #if score >= 60:
+                #print('Your grade is a D.')
+            #else:
+                #print('Your grade is an F.')
+
+#if score >= 90:
+#   print('Your grade is an A.')
+#elif score >= 80:
+#    print('Your grade is a B.')
+#elif score >= 70:
+#    print('Your grade is a C.')
+#elif score >= 60:
+#    print('Your grade is a D.')
+#else:
+#    print('Your grade is an F.')
+
+numbers = [0, 1, 2, 3, 4]
+
+for num in numbers:
+    print(num)
+
+for num in range(5):
+    print(num)
